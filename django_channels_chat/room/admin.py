fefa2e90d3c5fcd0eea3a1c65eebafe801bd06d5
oleh.zorenko <lovemybeard@gmail.com>
2023-08-24 23:39:05 +0300

    Add `list_display` to `Message` and `Room` admin models

--- django_channels_chat/room/admin.py
+++ django_channels_chat/room/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import Message, Room
 
 
-admin.site.register(Message)
-admin.site.register(Room)
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('get_room', 'user', 'content', 'date_added',)
+
+    @admin.display(description='Room')
+    def get_room(self, obj):
+        return obj.room.name
+
+    def get_queryset(self, request):
+        return Message.objects.select_related('user', 'room')
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
